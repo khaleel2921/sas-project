74f505a10764e3d7b4935ed104ef16505dd37b08
unknown <muhammedkhaleelpv@gmail.com>
2024-09-17 21:25:39 +0530

    first commit

--- employee_app/urls.py
+++ employee_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('',views.add_employee,name='add_employee')
+    path('',views.index,name='index.html'),
+    path('add_employee/',views.add_employee,name='add_employee'),
+    path('employee_list',views.employee_list,name='employee_list'),
+    path('search',views.searching,name='search'),
 ]
--- employee_app/views.py
+++ employee_app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,auth
+from  django.http import  HttpResponse
 from . models import employee
 from . forms import employeeForm
+from django.db.models import Q
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
 def add_employee(request):
     if request.method=="POST":
         employeeid=request.POST['employeeid']
@@ -13,11 +17,20 @@ def add_employee(request):
         contactnumber=request.POST['contactnumber']
         department=request.POST['department']
         designation=request.POST['designation']
-        if employee.objects.filter(email=email).exists():
-            return redirect('add_employee')
-        else:
-            user= employee.objects.create_user(employeeid=employeeid,firstname=firstname,lastname=lastname,email=email,contactnumber=contactnumber,department=department,designation=designation)
-            user.save()
-        print("Employee Id created")
+        user= employee(employeeid=employeeid,firstname=firstname,lastname=lastname,email=email,contactnumber=contactnumber,department=department,designation=designation)
+        user.save()
+        return HttpResponse('Employee Id created')
     else:
         return render(request,'add_employee.html')
+
+def employee_list(request):
+    object=employee.objects.all()
+    return render(request,"employee_list.html",{'list':object})
+
+def searching(request):
+    search=None
+    query=None
+    if 'q' in request.GET:
+        query=request.GET.get('q')
+        search=employee.objects.all().filter(Q(employeeid__contains=query)|Q(firstname__contains=query)|Q(department__contains=query))
+    return render(request,'search.html',{'qr':query,'sr':search})
